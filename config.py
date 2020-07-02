f00a0592240b60cc42b895ad194b0273485956d0
Jianwei Liang <jliang@hlpusd.k12.ca.us>
2020-07-02 12:18:25 -0700

    add static

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 import cherrypy
 
-path = os.path.abspath(os.path.dirname(__file__))
+path = os.path.abspath(os.getcwd())
 config = {
     'global': {
         'server.socket_host': '127.0.0.1',
@@ -12,13 +12,13 @@ config = {
 
         'tools.trailing_slash.on': False
     },
-    '/resource': {
-        'tools.staticdir.on': True,
-        'tools.staticdir.dir': os.path.join(path, 'public', 'resource')
-    },
     '/': {
         'tools.sessions.on': True,
         'tools.sessions.storage_class': cherrypy.lib.sessions.FileSession,
-        'tools.sessions.storage_path': os.path.join(path, 'sessions')
-    }
+        'tools.sessions.storage_path': os.path.join(path, 'sessions'),
+    },
+    '/resource': {
+        'tools.staticdir.on': True,
+        'tools.staticdir.dir': os.path.join(path, 'public')
+    },
 }
